222e98d0a374898f88af90d9b2b7ab741a4179e0
Teah Brumbaugh <fandomqueen707@gmail.com>
2025-04-12 20:43:43 -0700

    Individual Player Movement
    There is the Arrow Player (moves via left/right arrows) and AD Player (moves via a/d keys)

--- Spring Game Jam Project/Assets/Player/PlayerMovement.cs
+++ Spring Game Jam Project/Assets/Player/PlayerMovement.cs
@@ -1,54 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
-    private bool facingRight = true;
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _movementSpeed = 21f;
+    [SerializeField] private KeyCode _rightMoveKey;
+    [SerializeField] private KeyCode _leftMoveKey;
 
-    [SerializeField] private float jumpSpeed;
-    [SerializeField] private float speed;
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+    private float _horizontalDirection;
+    private bool _isFacingRight = true;
 
-    // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        if (IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-        }
-        Flip();
+
+        if (Input.GetKeyDown(_rightMoveKey))
+            _horizontalDirection = 1;
+        else if (Input.GetKeyDown(_leftMoveKey))
+            _horizontalDirection = -1;
+
+        // Note: See Task List document for Flip() issues
+        //Flip();
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        _rb.velocity = new Vector2(_horizontalDirection * _movementSpeed, _rb.velocity.y);
     }
 
-    private void Flip()
-    {
-        if (facingRight && horizontal < 0f || !facingRight && horizontal > 0f)
-        {
-           facingRight = !facingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-    }
-
-    private bool IsGrounded()
-    {
-        Debug.Log(Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer));
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-        
-    }
-
-    private void Jump()
-    {
-
-    }
+    //private void Flip()
+    //{
+    //    if (_isFacingRight && _horizontalDirection < 0f || !_isFacingRight && _horizontalDirection > 0f)
+    //    {
+    //        _isFacingRight = !_isFacingRight;
+    //        Vector3 localScale = transform.localScale;
+    //        localScale.x *= -1f;
+    //        transform.localScale = localScale;
+    //    }
+    //}
 }
